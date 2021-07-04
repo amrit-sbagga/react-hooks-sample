406247ec71d1310dbcc68cbc6aa48bb8d8dc97d0
Amrit Singh <amrit.sbagga@gmail.com>
2021-07-04 15:11:47 +0530

    useEffect usage in react hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Link , Route} from 'react-router-dom'
 
 function App() {
+  
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+const Home = () => {
+
+  const [inp, setInp] = useState(50); 
+
+  useEffect(() => {    
+    return () => {
+      //code cleanup
+      console.log("Home component unmounted..!!");
+    }
+  }, [])
+
+  useEffect(() => {
+    console.log("useEffect - conditonal rendering..!!");
+  }, [inp])
+
+  return (
+    <div>
+      <h3>This is home page</h3>
+      <h3>{inp}</h3>
+      <button onClick={()=>{  setInp(inp+1) }}>Increment inp</button>
+      <br/>
+      <Link to="/about">Go to about</Link>
+    </div>
+  )
+}
+
+const About = () => {
+
   const [name, setName] = useState("Amrit");
   const [age, setAge] = useState(31);
   const [no, setno] = useState(2);
@@ -12,21 +52,34 @@ function App() {
     setno((prevNo) => prevNo + 1);
   }
 
-  return (
-    <div className="App">
-     <h1> React Hooks</h1>
-     <h3>{name}</h3>
-     <h3>{age}</h3>
-     <h3>{no}</h3>
-     <button onClick={()=>setName("Monty")}>Change name</button>
-     <br/>
-     <button onClick={()=>setAge(age+1)}>Change age</button>
-     <br/>
-     <button onClick={()=>increment()}>Increment by 3</button>
-    </div>
-  );
-}
+  useEffect(() => {
+    console.log("useEffect called in about page, bahaves like componentDidMount()")
+    
+  }, [])
 
+  useEffect(() => {
+    //this will get called once at load time
+    //plus on every age increment
+    console.log("useEffect called in for age..!!") 
+  }, [age])
+
+  return (
+    <div>
+      <h3>This is about page</h3>
+      <h3> React Hooks</h3>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{no}</h3>
+      <button onClick={()=>setName("Monty")}>Change name</button>
+      <br/>
+      <button onClick={()=>setAge(age+1)}>Change age</button>
+      <br/>
+      <button onClick={()=>increment()}>Increment by 3</button>
+
+      <br/>
+    </div>
+  )
+}
 
 
 export default App;
